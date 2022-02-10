7cb679a72cf6bfbfe5e92bd9c17d2fee4b5e4c53
Ruhul Amin <ruhulaminjr@debian-BULLSEYE-live-builder-AMD64>
2022-02-10 17:28:48 +0600

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const App = express();
+App.use(express.json());
+
+module.exports = App;
---
+++ server/src/index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+const app = require("./app");
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("server running oon http://localhost:" + PORT);
+});

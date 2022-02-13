84d3c1c7b70daffe751636160087eb2ea3eba37f
Ruhul Amin <ruhulaminjr@debian-BULLSEYE-live-builder-AMD64>
2022-02-13 21:45:37 +0600

    added custom scripts

--- client/src/hooks/usePlanets.js
+++ client/src/hooks/usePlanets.js
@@ -13,7 +13,7 @@ function usePlanets() {
   useEffect(() => {
     getPlanets();
   }, [getPlanets]);
-
+  console.log(planets);
   return planets;
 }
 
--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const path = require("path");
+const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = require("./app");
 const { loadPlanets } = require("./models/planets.models");
@@ -6,6 +8,10 @@ const { loadPlanets } = require("./models/planets.models");
 const server = http.createServer(app);
 const plantetRouters = require("./controller/routes/planets.router");
 app.use(plantetRouters);
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 loadPlanets().then(() => {
   server.listen(PORT, () => {
     console.log("server running oon http://localhost:" + PORT);
